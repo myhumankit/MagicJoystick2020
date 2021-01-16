ea26faf2776ac447fefa0b5773ee5b20a06700f6
fredgarr <frederic.garreau.1@gmail.com>
2021-01-16 17:34:39 +0100

    [MITM] Add "pican2 Dual" support

--- RnetMitm/RnetIntercept.py
+++ RnetMitm/RnetIntercept.py
@@ -22,14 +22,9 @@ TCP_BUFFER_SIZE = 1024
 
 
 """
-Main R-net class to send joystick frames on the bus
-and take the control over the legacy JSM
-
-input: "jsm_address" is the identification of the wheelchair JSM
-                    this is specific to each JSM and has to be tuned
-                    example : '02001100' for our test wheelchair
+R-net class for two raspi and pican single port logging
 """
-class RnetLogger(threading.Thread):
+class RnetIPLogger(threading.Thread):
 
     def __init__(self, rIp, rPort, logger_tag, logfile):
         self.cansocket = None
@@ -45,7 +40,7 @@ class RnetLogger(threading.Thread):
         try:
             self.cansocket = can2RNET.opencansocket(0)
         except :
-            logger.error("socketcan cannot be opened! Check connectivity")
+            logger.error("socketcan can0 cannot be opened! Check connectivity")
             sys.exit(1)
 
 
@@ -53,10 +48,13 @@ class RnetLogger(threading.Thread):
         can2RNET.cansendraw(self.cansocket, data)
 
 
+    """
+    Used for 2 raspi config with IP link
+    """
     def start_daemon(self):
 
         # launch daemon
-        logger.debug("Starting Rnet daemon")
+        logger.debug("Starting IP Rnet daemon")
         daemon = threading.Thread(target=self.rnet_daemon, daemon=True)
         daemon.start()
         return daemon
@@ -86,6 +84,61 @@ class RnetLogger(threading.Thread):
             rnetFrame = can2RNET.canrecv(self.cansocket)
             self.logfile.write('%s: %r\n' %(self.logger_tag,can2RNET.dissect_frame(rnetFrame)))
             self.sock.send(rnetFrame)
+
+
+"""
+R-net class for single raspi and pican dual port logging
+"""
+class RnetDualLogger(threading.Thread):
+
+    def __init__(self, tag0, tag1, logfile):
+        self.cansocket0 = None
+        self.cansocket1 = None
+        self.tag0 = tag0
+        self.tag1 = tag1
+        self.logfile = logfile
+        
+        threading.Thread.__init__(self)
+        logger.info("Opening socketcan")
+        try:
+            self.cansocket0 = can2RNET.opencansocket(0)
+        except :
+            logger.error("socketcan can0 cannot be opened! Check connectivity")
+            sys.exit(1)
+        try:
+            self.cansocket1 = can2RNET.opencansocket(1)
+        except :
+            logger.error("socketcan can1 cannot be opened! Check connectivity")
+            sys.exit(1)
+
+
+    """
+    used for single raspi and pican dual config
+    """
+    def start_daemons(self):
+
+        # launch daemon
+        logger.debug("Starting Rnet Dual daemons")
+        daemon0 = threading.Thread(target=self.rnet_daemon, args=[self.cansocket0, self.cansocket1, self.tag0], daemon=True)
+        daemon0.start()
+        daemon1 = threading.Thread(target=self.rnet_daemon, args=[self.cansocket1, self.cansocket0, self.tag1], daemon=True)
+        daemon1.start()
+        return daemon0, daemon1
+
+
+    """
+    Endless loop waiting for Rnet frames
+    Each frame is logged and forwarded to remote server
+    """
+    def rnet_daemon(self, listensock, sendsock, logger_tag):
+        logger.debug("Rnet listener daemon started")
+        
+        # sending joystick frame @100Hz
+        while True:
+            rnetFrame = can2RNET.canrecv(listensock)
+            logger.info('%s: %r\n' %(logger_tag,can2RNET.dissect_frame(rnetFrame)))
+            self.logfile.write('%s: %r\n' %(logger_tag,can2RNET.dissect_frame(rnetFrame)))
+            can2RNET.cansendraw(sendsock, rnetFrame)
 
 
 """
@@ -137,13 +190,55 @@ def parseInputs():
     parser.add_argument("--lPort", type=int, default="4146", help="Defines local server port to listen, default is 4146")
     parser.add_argument("--rIp", type=str, default="192.168.1.145", help="Defines remote IPv4 address to connect, default is '192.168.1.145'")
     parser.add_argument("--rPort", type=int, default="4145", help="Defines remote port to connect, default is 4145")
-    parser.add_argument("--cantag", type=str, default="JSM", help="Defines logger tag like 'JSM' for CAN stream. Default is 'JSM'")
-    parser.add_argument("--iptag", type=str, default="MOTOR", help="Defines logger tag like 'MOTOR' for IP stream. Default is 'MOTOR'")
+    parser.add_argument("--cantag", type=str, default="JSM", help="Defines logger tag like 'JSM' for Rx-CAN0 stream. Default is 'JSM'")
+    parser.add_argument("--iptag", type=str, default="MOTOR", help="Defines logger tag like 'MOTOR' for Rx-IP or Rx-CAN1 stream. Default is 'MOTOR'")
     parser.add_argument("--logfile", type=str, default="logfile.txt", help="Output can log filename")
     parser.add_argument("-d", "--debug", help="Enable debug messages", action="store_true")
+    parser.add_argument("--dual", help="'PICAN2 Dual' board case, no need to have 2 raspi. IP/port inputs not required", action="store_true")
 
 
     return parser.parse_args()
+
+
+"""
+Configuration using two raspi with single port pican boards.
+"""
+def picanCase(args):
+    # Start Rnet listener daemon
+    with open(args.logfile,"w") as logfile:
+
+        # Connect and initialize Rnet controller
+        # Will listen for Rnet frames and transmit
+        # them to distant TCP server
+        rnet = RnetIPLogger(args.rIp,args.rPort, args.cantag, logfile)
+        serv = server(args.lIp, args.lPort, args.iptag, rnet, logfile)
+
+        # Start position daemon
+        rnet.start_daemon()
+
+        # Start TCP listener daemon
+        # Will listen for TCP frames and transmit
+        # them on Rnet bus
+        serv.start()
+
+
+"""
+Configuration using one raspi with dual port pican boards.
+"""
+def picanDualCase(args):
+    # Start Rnet listener daemon
+    with open(args.logfile,"w") as logfile:
+
+        # Connect and initialize Rnet controller
+        # Will listen for Rnet frames and transmit
+        # them to distant TCP server
+        rnet = RnetDualLogger(args.cantag, args.iptag, logfile)
+        daemon0, daemon1 = rnet.start_daemons()
+
+        daemon0.join()
+        daemon1.join()
+
+
 
 if __name__ == "__main__":
 
@@ -153,21 +248,8 @@ if __name__ == "__main__":
     if args.debug:
         logger.setLevel(logging.DEBUG)
 
-    # Start Rnet listener daemon
-    with open(args.logfile,"w") as logfile:
-
-        # Connect and initialize Rnet controller
-        # Will listen for Rnet frames and transmit
-        # them to distant TCP server
-        rnet = RnetLogger(args.rIp,args.rPort, args.cantag, logfile)
-        serv = server(args.lIp, args.lPort, args.iptag, rnet, logfile)
-
-        # Start position daemon
-        deamon = rnet.start_daemon()
-
-        # Start TCP listener daemon
-        # Will listen for TCP frames and transmit
-        # them on Rnet bus
-        serv.start()
-        
+    if args.dual is False:
+        picanCase(args)
+    else:
+        picanDualCase(args)
 
