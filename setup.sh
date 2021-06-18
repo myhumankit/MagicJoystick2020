72979a76565fe137a3c2a0ed659dbe700772bafd
fredgarr <frederic.garreau.1@gmail.com>
2021-06-18 20:19:32 +0200

    Update setup script to support pican dual board

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # This setup will install all packages and configure the raspberry-pi
-# to run MagicJoy software
+# to run MagicJoy software with a Pican-Dual board.
 #
 # THIS SCRIPT MUST BE RUN WITH: sudo setup.sh
 #
@@ -28,6 +28,7 @@ echo "# MagicJoy config starts ============="  >> /boot/config.txt
 echo "dtparam=i2c_arm=on" >> /boot/config.txt
 echo "dtparam=spi=on"  >> /boot/config.txt
 echo "dtoverlay=mcp2515-can0,oscillator=16000000,interrupt=25"  >> /boot/config.txt
+echo "dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25"  >> /boot/config.txt
 echo "dtoverlay=spi-bcm2835"  >> /boot/config.txt
 echo "dtoverlay=spi1-1cs"  >> /boot/config.txt
 echo "# MagicJoy config stops ============="  >> /boot/config.txt
@@ -36,9 +37,16 @@ echo "Updating /etc/network/interfaces ..."
 # ==============================================
 cp /etc/network/interfaces /etc/network/interfaces.bak
 echo "# MagicJoy config starts ============="  >> /etc/network/interfaces
-echo "# pican interface configuration "  >> /etc/network/interfaces
+echo "# pican interface0 configuration "  >> /etc/network/interfaces
 echo "allow-hotplug can0" >> /etc/network/interfaces
 echo "iface can0 can static" >> /etc/network/interfaces
+echo "       bitrate 125000" >> /etc/network/interfaces
+echo "       down /sbin/ip link set $IFACE down" >> /etc/network/interfaces
+echo "       up /sbin/ip link set $IFACE up" >> /etc/network/interfaces
+echo ""  >> /etc/network/interfaces
+echo "# pican interface1 configuration "  >> /etc/network/interfaces
+echo "allow-hotplug can1" >> /etc/network/interfaces
+echo "iface can1 can static" >> /etc/network/interfaces
 echo "       bitrate 125000" >> /etc/network/interfaces
 echo "       down /sbin/ip link set $IFACE down" >> /etc/network/interfaces
 echo "       up /sbin/ip link set $IFACE up" >> /etc/network/interfaces
@@ -55,7 +63,7 @@ echo "# MagicJoy config stops ============="  >> /etc/modules
 
 echo "running apt-update and installing required modules ..."
 # ===========================================================
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install can-utils build-essential python3-dev python3-smbus git python3-pip cmake
 echo "running pip3 and installing required modules ..."
 # ===========================================================
@@ -68,6 +76,14 @@ pip3 -y install spidev
 
 echo "Installing can2RNET python library"
 python3 ./can2RNET/setup.py install
+
+echo "Installing python construct"
+cd Depends 
+tar -xf construct-2.10.63.tar.gz
+cd construct-2.10.63
+python3 setup.py install
+cd ..
+rm -rf construct-2.10.63
 
 echo "Setup done"
 echo ""
