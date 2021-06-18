905e215a6ff4065090f6c000929ac31b479032cd
fredgarr <frederic.garreau.1@gmail.com>
2021-06-18 20:18:18 +0200

    Massive RnetCtrl clean up

--- RnetCtrl/RnetCtrl.py
+++ RnetCtrl/RnetCtrl.py
@@ -9,60 +9,15 @@ import struct
 import logging
 import binascii
 from can2RNET import can2RNET
-
+from can2RNET.RnetDissector import RnetDissector
+import RnetCtrlInit
 
 logger = can2RNET.logger
 
 
 # Default constants:
-ID_JOY_CONTROL    = '02001100'  # may differ according to your setup
-ID_JOY_NULL       = '02000200'  # may differ according to your setup, not used
-RNET_XY_FRAME_HEADER = '02000000:FF000000'
-NEUTRAL_JOY_POSITION = '00.00'
-
+NEUTRAL_JOY_POSITION = '%d.%d' %(0,0)
 TCP_BUFFER_SIZE = 1024
-
-
-
-"""
-convert dec to hex with leading 0s and no '0x'
-"""
-def dec2hex(dec, hexlen):
-    h = hex(int(dec))[2:]
-    l = len(h)
-    if h[l-1] == "L":
-        l -= 1  # strip the 'L' that python int sticks on
-    if h[l-2] == "x":
-        h = '0'+hex(int(dec))[1:]
-    return ('0'*hexlen+h)[l:l+hexlen]
-
-
-"""
-Basic commands over Rnet 
-"""
-RNET_DICT={
-    'JSM_CONNECT'          : '00c#',
-    'JSM_SERIAL'           : '00e#9700148100000000',
-    'JSM_SERIAL_REQ'       : '7b3#',
-    'JMS_CFG_MODE1_REQ'    : '7b1#',
-    'JSM_UI_READY'         : '1c240101#',      # 1c24XX01, XX=device
-    'JSM_SPEED_CFG'        : '0a041100#00',    # Use JSM identifier '1100' same used for joystick position, and set to minimum speed
-    'JSM_JOY_POS'          : '02001100#0000',  # Set joystick osition to 00
-    'JSM_HEARTBEAT'        : '03c30f0f#87878787878787',
-
-    'PM_SERIAL_ACK'        : '0007b3#R',
-    'PM_CFG_MODE1_ACK'     : '0007b0#',
-    'PM_BATTERY_LEVEL'     : '1C0CXX00#YY',    # XX=device, YY=power level in %. yy range is [0x00 - 0x64]. Periodic: 1000ms
-    'PM_HEARTBEAT'         : '0c140000#XX',
-    'PM_CONNECTED'         : '0c280000#00',    # Sent only once after serial exchange
-}
-
-
-
-SERIAL_PERIOD = 0.05
-SERIAL_REQ_PERIOD = 0.03
-CONFIG_TIMEOUT = 0.2
-HEARTBEAT_PERIOD = 0.1
 
 
 """
@@ -70,10 +25,15 @@ JMS PowerOn class will substitute to a legacy JSM
 So you can poweron raspberry witout the need to 
 poweron the JMS
 """
-class JsmPowerOn(threading.Thread):
+class JsmDeamons(threading.Thread):
 
-    def __init__(self, cansocket):
+    SERIAL_PERIOD = 0.05
+    HEARTBEAT_PERIOD = 0.1
+
+    def __init__(self, cansocket, jsm_serial):
         self.cansocket = cansocket
+        self.jsm_serial = RnetDissector.rnet_serial(jsm_serial)
+        self.heartbeat = RnetDissector.rnet_heartbeat()
         threading.Thread.__init__(self)
 
 
@@ -83,8 +43,8 @@ class JsmPowerOn(threading.Thread):
     def serialDaemon(self):
         logger.info("<Serial number send daemon started>")
         while True:
-            time.sleep(SERIAL_PERIOD)
-            can2RNET.cansend(self.cansocket, RNET_DICT['JSM_SERIAL'])
+            time.sleep(self.SERIAL_PERIOD)
+            can2RNET.cansendraw(self.cansocket,self.jsm_serial.encode())
 
 
     def startSerialDaemon(self):
@@ -93,11 +53,14 @@ class JsmPowerOn(threading.Thread):
         return daemon
 
 
+    """
+    Sending periodic 100ms heartbeat <Thread>
+    """
     def heartbeatDaemon(self):
         logger.info("<Heartbeat daemon started>")
         while True:
-            time.sleep(HEARTBEAT_PERIOD)
-            can2RNET.cansend(self.cansocket, RNET_DICT['JSM_HEARTBEAT'])
+            time.sleep(self.HEARTBEAT_PERIOD)
+            can2RNET.cansendraw(self.cansocket, self.heartbeat.encode())
 
 
     def startHeartbeatDaemon(self):
@@ -106,192 +69,71 @@ class JsmPowerOn(threading.Thread):
         return daemon
 
 
-    def ListenerDaemon(self):
-        logger.info("<Listener daemon started>")
-        data = 0
-        while True:
-            data = can2RNET.canrecvTimeout(self.cansocket, 0.1)
-            time.sleep(0.01)
-            if data is not b'':
-                logger.debug("**** Recv controller data: %r ****" %can2RNET.dissect_frame(data))
-
-
-    def startListenerDaemon(self):
-        daemon = threading.Thread(target=self.ListenerDaemon, daemon=True)
-        daemon.start()
-        return daemon
-
-
-
-    def recTimeOut(self, timeout):
-        data = 0
-        while data is not b'':
-            data = can2RNET.canrecvTimeout(self.cansocket, timeout)
-            if data is not b'':
-                logger.info("BOOT Recv controller data: %r" %can2RNET.dissect_frame(data))
-
-
-    def reqack(self, req, ack):
-
-        can2RNET.cansend(self.cansocket, req)
-        ack_frame = can2RNET.canrecvTimeout(self.cansocket, SERIAL_REQ_PERIOD)
-
-        pm_answer = ''
-
-        if ack_frame is not b'':
-            pm_answer = can2RNET.dissect_frame(ack_frame)
-            logger.debug("Wait for PM %s, got : %s" %(ack, pm_answer))
-        else:
-            logger.debug("Wait for PM %s, got : TIMEOUT" %(req))
-
-        while pm_answer != ack:
-            can2RNET.cansend(self.cansocket, req)
-            ack_frame = can2RNET.canrecvTimeout(self.cansocket, SERIAL_REQ_PERIOD)
-
-            if ack_frame is not b'':
-                pm_answer = can2RNET.dissect_frame(ack_frame)
-                logger.debug("Wait for PM %r, got : %r" %(ack, pm_answer))
-            else:
-                logger.debug("Wait for PM %s, got : TIMEOUT" %(req))
-
-
-    def powerOn(self):
-
-        # Send JSM wake up to PM
-        can2RNET.cansend(self.cansocket, RNET_DICT['JSM_CONNECT'])
-        time.sleep(0.2)
-
-        # start periodic serial info send thread:
-        self.startSerialDaemon()
-
-        # Initiate serial exchange, and wait for ack from PM
-        self.reqack(RNET_DICT['JSM_SERIAL_REQ'], RNET_DICT['PM_SERIAL_ACK'])        
-
-        logger.info("BOOT: get serial ack from pm")
-
-        # Receive serial frames from PM  
-        # assuming a 2000ms timeout PM tells serial has benn sent
-        self.recTimeOut(CONFIG_TIMEOUT)
-
-        logger.info("\n\nBOOT: RX TIMEOUT - Controller sent all configuration\n")
-
-        # Tell PM that JSM UI is ready and wait for PM connected answer
-
-        can2RNET.cansend(self.cansocket, RNET_DICT['JSM_UI_READY'])
-
-        logger.info("BOOT: get pm connected !")
-
-        # Set speed to minimum 
-        can2RNET.cansend(self.cansocket, RNET_DICT['JSM_SPEED_CFG'])
-
-        self.recTimeOut(CONFIG_TIMEOUT)
-
-        # Logger daemon for frames received from Controller
-        self.startListenerDaemon()
-
-        # Start heartbeat daemon thread
-        self.startHeartbeatDaemon()
-
 
 
 """
 Main R-net class to send joystick frames on the bus
 and take the control over the legacy JSM
 
-input: "jsm_address" is the identification of the wheelchair JSM
-                    this is specific to each JSM and has to be tuned
-                    example : '02001100' for our test wheelchair
 """
 class RnetControl(threading.Thread):
 
-    # Constants :
-    FRAME_JSM_INDUCE_ERROR = '0c000000#'
     POSITION_FREQUENCY = 0.01   # 100Hz
-    NB_XY_FRAME = 20 
 
-    def __init__(self, jsm_address, testmode = False):
+    def __init__(self, testmode = False):
         self.joy_x = 0
         self.joy_y = 0
         self.cansocket = None
-        self.jsm_address = jsm_address
         self.testmode = testmode
         threading.Thread.__init__(self)
         logger.info("Opening socketcan")
-        try:
-            if testmode is False:
-                self.cansocket = can2RNET.opencansocket(0)
-                self.cansend = can2RNET.cansend
-            else:
-                logger.warning("Switching in test mode, do not connect to CAN bus")
-                self.cansend = self.dummy
-        except :
-            logger.error("socketcan cannot be opened! Check connectivity")
-            sys.exit(1)
 
-        self.powerOn = JsmPowerOn(self.cansocket)
+        if testmode is False:
+            self.cansend = can2RNET.cansend
+        else:
+            logger.warning("Switching in test mode, do not connect to CAN bus")
+            self.cansend = self.dummy
 
+        # Open can socket to prepare fake JSM PowerOn
+        self.jsm =  RnetCtrlInit.JSMiser()
+        self.cansocket = self.jsm.motor_cansocket
+        self.joyPosition = RnetDissector.rnet_joyPosition(0,0,self.jsm.jsm_subtype)
+
+
+    def powerOn(self):
+        self.jsmDaemons = JsmDeamons(self.cansocket, self.jsm.jsm_serial)
+        # Send power on sequence (Sends all JSM init frames)
+        self.jsm.jsm_start()
+
+        # Send MaxSpeed frame. Max speed set to 20%:
+        speed = RnetDissector.rnet_motorMaxSpeed(20, self.jsm.jsm_subtype)
+        can2RNET.cansendraw(self.cansocket, speed.encode())
+
+        # activate periodic messages to motor
+        self.jsmDaemons.startHeartbeatDaemon()
+        self.jsmDaemons.startSerialDaemon()
 
 
     def dummy(self, arg0, arg1):
         pass
-
-    
-    """
-    WIP : autodetect frame address
-    """
-    def get_xy_frame_id(self, frame):
-        return frame[4:8]
-
-    def get_xy_frame_values(self, frame):
-        x = struct.unpack("I", frame[:4])[0]
-
-        return frame.split('#')[1]
-
-    """
-    WIP : This function will try to identify audomatically the current JSM address
-    """
-    def get_jsm_header(self):
-
-        positionFrames = []       
-
-        for _ in range(self.NB_XY_FRAME):
-            positionFrames.append(can2RNET.canwait(rnet.cansocket, RNET_XY_FRAME_HEADER))
-
-
-    """
-    This function will send 5 frames to the JSM and will crash it
-    This is necessary to take the control on the wheel chair
-    Idealy this function should not be send...
-    """
-    def send_jsm_exploit(self):
-        logger.debug("Inducing JSM error by sending 5 frames of type '0c000000#'")
-        # send in less than 1ms theses frames to induce JSM error
-        for _ in range(5):
-            self.cansend(self.cansocket, self.FRAME_JSM_INDUCE_ERROR)
 
 
     """
     Function to be called when a new position is received from positionning client
     """
     def update_joy_position(self, data):
-        x = str(data).split('.')[0]
-        y = str(data).split('.')[1]
+        x = int((data).split('.')[0])
+        y = int((data).split('.')[1])
         logger.debug("Update position: x=%r, y=%r" %(x,y))
-        self.joy_x = x
-        self.joy_y = y
+        self.joyPosition.set_data(x, y)
 
-
-    def createJoyFrame(self, id_control, joystick_x, joystick_y):
-        joyframe = id_control+'#' + \
-            dec2hex(joystick_x, 2)+dec2hex(joystick_y, 2)
-        return joyframe
 
     """
     Function to be called periodically to send new joy command to Rnet bus
     """
     def send_joy_position(self):
-        joyframe = self.createJoyFrame(ID_JOY_CONTROL, self.joy_x, self.joy_y)
-        self.cansend(self.cansocket, joyframe)
+        joyframe = self.joyPosition.encode()
+        can2RNET.cansendraw(self.cansocket, joyframe)
 
 
     def start_daemon(self):
@@ -312,8 +154,6 @@ class RnetControl(threading.Thread):
             time.sleep(self.POSITION_FREQUENCY)
 
 
-
-
 class server():
 
     def __init__(self, ip, port, rnet):
@@ -327,6 +167,7 @@ class server():
         except:
             logger.error("binding socket to %s:%d failed" %(self.ip, self.port))
             sys.exit(1)
+
 
     def start(self):         
             
@@ -353,9 +194,8 @@ Argument parser definition
 """
 def parseInputs():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="0.0.0.0", help="Define server IPv4 address to listen, default is '0.0.0.0'")
+    parser.add_argument("--ip", type=str, default="0.0.0.0", help="Define server IPv4 address to listen to receive Joy positions, default is '0.0.0.0'")
     parser.add_argument("--port", type=int, default="4141", help="Define server portto listen, default is 4141")
-    parser.add_argument("--rnetaddr", type=str, default=ID_JOY_CONTROL, help="Define Rnet wheelchair JSM address, default is %s" %ID_JOY_CONTROL)
     parser.add_argument("-d", "--debug", help="Enable debug messages", action="store_true")
     parser.add_argument("-t", "--test", help="Test mode, do not connect to CAN bus", action="store_true")
 
@@ -372,9 +212,12 @@ if __name__ == "__main__":
 
     # Connect and initialize Rnet controller,
     # start heartbeat
-    rnet = RnetControl(args.rnetaddr, args.test)
+    rnet = RnetControl(args.test)
 
-    rnet.powerOn.powerOn()
+    # Send JSM init sequence 'power on'
+    rnet.powerOn()
+
+
     # Start Rnet controller
     try:
         # Start position daemon
